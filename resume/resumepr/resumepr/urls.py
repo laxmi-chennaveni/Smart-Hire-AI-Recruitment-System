31f6028584ca2b8ea51ac1f08f56c12c425c3922
Laxmi Chennaveni <chennavenilaxmi14@gmail.com>
2026-06-29 00:00:28 +0530

    Update URL patterns and add static file serving

--- resume/resumepr/resumepr/urls.py
+++ resume/resumepr/resumepr/urls.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>Smart Hire AI is Live! 🚀</h1>")
+from django.conf import settings
+from django.conf.urls.static import static
+from demoapp import views
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
+    path('', views.welcome_view, name='welcome'),
+    path('login/', vuews.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('post_job/', views.post_job_view, name='post_job'),
+    path('job/apply/<init:job_id>/', views.apply_job_view, name='apply_job')
+    path('job/<int:job_id>/applicants/', views.view_applicants, name='view_applicants'),
+    path('application/<int:app_id>/status/<str:status>/', views.update_status, name='update_status'),
+    path('job/<int:job_id>/ai-screen/', views.run_ai_screening, name='run_ai_screening'),
 ]
+
+#
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root-settings.MEDIA_ROOT)
+
+
+
